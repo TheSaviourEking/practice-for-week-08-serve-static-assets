a32bb33f79ea77c1af37a8ef063888addb8cfd5d
TheSaviourEking <saviour.eking@outlook.com>
2024-01-17 21:19:54 +0100

    [chore] implemented the static files endpoints in server.js
    this will:
       - let the server serve static files too

--- server.js
+++ server.js
@@ -2,7 +2,38 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here
+    // Your code here
+    
+    // GET /static
+    if (req.method === 'GET' && req.url.startsWith('/static')) {
+	let reqDataPath = req.url.split('/');
+	const dirName = './assets';
+	const fileName = reqDataPath.slice(-1).join('');
+
+	let fileContents = null;
+	res.statusCode = 200;
+	
+	// image
+	if (fileName.includes('.jpg')) {
+	    fileContents = fs.readFileSync(dirName + '/images/' + fileName);	    
+	    res.setHeader('content-Type', 'image/jpeg');
+	}
+
+	// css
+	if (fileName.includes('.css')) {
+	    fileContents = fs.readFileSync(dirName + '/css/' + fileName);
+
+	    res.setHeader('content-Type', 'text/css');
+	}
+	
+	return res.end(fileContents, 'utf-8');
+    }
+    
+    const fileContents = fs.readFileSync('./index.html', 'utf-8');
+
+    res.statusCode = 200;
+    res.setHeader('content-Type', 'text/html');
+    return res.end(fileContents, 'utf-8');
 });
 
 const port = 5000;
